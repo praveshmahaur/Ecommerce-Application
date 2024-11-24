6aaa91dbd4e94daae76f9bb7450b97ebe5e13e2d
Pravesh mahaur <praveshmahaur328024@gmail.com>
2024-11-25 00:35:14 +0530

    password bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,31 +1,32 @@
 const express = require("express");
 const User = require("../model/usermodel");
+const bcryptjs = require("bcryptjs");
 
 const authRouter = express.Router();
 
 authRouter.post('/api/signup', async(req, res)=>{
     try{
-        // const {name, email, password, phone} = req.body;
-        const {email} = req.body;
+        const {email,name,phone,password} = req.body;
 
         const existingUser = await User.findOne({email})
         if(existingUser){
             return res.status(400).json({msg:'User email is Already Exist'})
         }
 
-        // let user = new User({
-        //     email,
-        //     password,
-        //     name,
-        //     phone,
-        // });
+        const hashPassword = await bcryptjs.hash(password,8);
 
-        // user = await user.save();
-        // res.json(user);
+        let user = new User({
+            email,
+            password: hashPassword,
+            name,
+            phone,
+        });
+        user = await user.save();
+        res.json(user);
 
-        const user = new User(req.body);
-        const saveUser = await user.save();
-        res.json(saveUser);
+        // const user = new User(req.body);
+        // const saveUser = await user.save();
+        // res.json(saveUser);
         
     }
     catch(err){
@@ -43,7 +44,6 @@ authRouter.get('/api/signup', async(req, res)=>{
         res.status(500).send(err);
     }
 });
-
 
 
 module.exports = authRouter;
