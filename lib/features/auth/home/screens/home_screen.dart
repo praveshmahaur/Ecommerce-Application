9e923bbd287e28b3d0221aa6494f3aa053785a08
Pravesh mahaur <praveshmahaur328024@gmail.com>
2025-01-08 10:56:44 +0530

    design homeScreen appbar/addressbox/categories

--- lib/features/auth/home/screens/home_screen.dart
+++ lib/features/auth/home/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_application/constants/global_colors.dart';
+import 'package:ecommerce_application/features/auth/home/widgets/address_box.dart';
 import 'package:ecommerce_application/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,10 +18,64 @@ class _HomeScreenState extends State<HomeScreen> {
     final user = Provider.of<UserProvider>(context).user;
 
     return Scaffold(
-      body:
-      Center(child: Text(
-        user.toJson(),
-        ),),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(55),
+        child: AppBar(
+              flexibleSpace: Container(
+                decoration:
+                    const BoxDecoration(gradient: GlobalColors.appBarGradient),
+              ),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 42,
+                      margin: const EdgeInsets.only(left: 15),
+                      child: Material(
+                        borderRadius: BorderRadius.circular(8),
+                        elevation: 1,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: InkWell(
+                              onTap: (){},
+                              child: Icon(Icons.search, color: Colors.black,size: 22,),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: const EdgeInsets.only(top: 10),
+                            border: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(7)),
+                              borderSide: BorderSide.none,
+                            ),
+                            enabledBorder: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(7)),
+                              borderSide: BorderSide(color: Colors.black38, width: 1),
+                            ),
+                            hintText: 'Search Amazon.in',
+                            hintStyle: const TextStyle(fontWeight: FontWeight.w400,fontSize: 17, color: Colors.black54)
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.transparent,
+                    height: 42,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    child: const Icon(Icons.mic, size: 25,),
+                  ) 
+                ],
+              ),
+            ),
+      ),
+      body:Column(
+        children: [
+          const AddressBox(),
+          Text(user.toJson())
+        ],
+      )
+      
     );
   }
 }
